001c7265cb72bac3a72f59c793735c8339e093aa
Đoàn Thị Thu Quyên <thuquyentntl@gmail.com>
2024-06-03 20:41:04 +0700

    update view store

--- aims_project_javafx/src/main/java/globalict/ViewStoreController.java
+++ aims_project_javafx/src/main/java/globalict/ViewStoreController.java
@@ -2,7 +2,6 @@ package globalict;
 
 import java.io.IOException;
 
-import globalict.aims.cart.Cart;
 import globalict.aims.store.Store;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -28,20 +27,46 @@ public class ViewStoreController
     
     @FXML
   
+    // public void initialize() {
+    //    // final String ITEM_FXML_FILE_PATH = "src\main\resources\globalict\Item.fxml";
+    //     int column = 0;
+    //     int row = 1;
+    //     for (int i=0;i<store.getItemsInStore().size();i++){
+    //         try {
+    //             FXMLLoader fxmlLoader = new FXMLLoader();
+    //             fxmlLoader.setLocation(getClass().getResource("Item.fxml"));
+    //             ItemController itemController = new ItemController(store);
+    //             fxmlLoader.setController(itemController);
+    //             AnchorPane anchorPane = new AnchorPane();
+    //             anchorPane = fxmlLoader.load();
+    //             itemController.setData(store.getItemsInStore().get(i));
+
+    //             if (column ==3){
+    //                 column =0;
+    //                 row++;
+    //             }
+    //             gridPane.add(anchorPane, column++, row);
+    //             GridPane.setMargin(anchorPane, new Insets(20,10,10,10));
+    //         } catch (IOException e) {
+    //             e.printStackTrace();
+    //         }
+    //     }
+          
+
+
+    // }
     public void initialize() {
-       // final String ITEM_FXML_FILE_PATH = "src\main\resources\globalict\Item.fxml";
         int column = 0;
         int row = 1;
         for (int i=0;i<store.getItemsInStore().size();i++){
             try {
                 FXMLLoader fxmlLoader = new FXMLLoader();
                 fxmlLoader.setLocation(getClass().getResource("Item.fxml"));
-                ItemController itemController = new ItemController(store);
+                ItemController itemController = new ItemController(store); // Truyền đối tượng Store vào hàm tạo của ItemController
                 fxmlLoader.setController(itemController);
-                AnchorPane anchorPane = new AnchorPane();
-                anchorPane = fxmlLoader.load();
+                AnchorPane anchorPane = fxmlLoader.load();
                 itemController.setData(store.getItemsInStore().get(i));
-
+    
                 if (column ==3){
                     column =0;
                     row++;
@@ -52,8 +77,6 @@ public class ViewStoreController
                 e.printStackTrace();
             }
         }
-          
-
-
     }
+    
 }
